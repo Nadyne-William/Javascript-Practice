427888f1ed50a3c43826814fee6be9df450269e5
Nadyne-William <frankekong33@gmail.com>
2022-10-13 21:44:59 +0100

    Practiced Loops

---
+++ JavaScript Demo/loopsPractice.js
@@ -0,0 +1,61 @@
+// for(var counter = 1; counter <= 100; counter++){
+//     console.log(counter)
+// }
+
+// var n = 6;
+
+// for(var counter = 1; counter <= n; counter++){
+//     console.log(counter)
+// }
+
+//display sum of natural numbers below
+
+// var sum =0;
+
+// for(var counter = 1; counter <= 3; counter++){
+//     sum += counter;
+    
+// }
+
+// console.log(sum);
+
+//counting backwards
+
+// for (var counter = 10; counter >= 1; counter -- ){
+//     console.log(counter)
+// }
+
+// var counter = 0;
+// var sum = 0;
+
+// while(counter < 10){
+//     counter++;
+//     if(counter % 2 == 0){
+//         sum += counter;
+//     }
+// }
+
+// console.log(sum)
+
+// let sum = 0;
+
+// let number = parseInt(prompt("Enter a number: "));
+
+// while(number >= 0) {
+
+//     // add all positive numbers
+//     sum += number;
+
+//     // take input again if the number is positive
+//     number = parseInt(prompt("Enter a number: "));
+// }
+
+// // display the sum
+// console.log(`The sum is ${sum}.`);
+
+for (var counter = 0; counter <= 50; counter++){
+    if(counter % 5 == 0){
+        console.log(counter)
+    }
+    // console.log(counter)
+}
